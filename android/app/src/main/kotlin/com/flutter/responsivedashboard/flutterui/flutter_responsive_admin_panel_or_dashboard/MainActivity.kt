423fe1d4104e1a456dfcb127b980adcefe924bb1
Chetan Vaghela <chetanvaghela457@gmail.com>
2024-08-07 02:26:11 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/flutter/responsivedashboard/flutterui/flutter_responsive_admin_panel_or_dashboard/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.flutter.responsivedashboard.flutterui.flutter_responsive_admin_panel_or_dashboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
